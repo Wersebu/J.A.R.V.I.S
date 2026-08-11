cd443e9de0c2e58a575788fe47120ab17560a0bf
Krzysztof Frynas <cz.damax@gmail.com>
2026-08-11 23:04:06 +0200

    Fixing WEB tool

--- jarvis-memory/src/main/java/com/jarvis/memory/pipeline/MainModelIntegratedToolTrigger.java
+++ jarvis-memory/src/main/java/com/jarvis/memory/pipeline/MainModelIntegratedToolTrigger.java
@@ -4,6 +4,7 @@ import com.jarvis.tools.runtime.ToolIntent;
 import com.jarvis.tools.runtime.ToolIntentDetector;
 import com.jarvis.tools.runtime.InformationFreshness;
 import com.jarvis.tools.runtime.InformationFreshnessEvaluator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -30,6 +31,7 @@ public class MainModelIntegratedToolTrigger implements ToolTriggerStrategy {
      * @param toolIntentDetector lightweight advisory detector
      * @param freshnessEvaluator request freshness classifier
      */
+    @Autowired
     public MainModelIntegratedToolTrigger(ToolIntentDetector toolIntentDetector, InformationFreshnessEvaluator freshnessEvaluator) {
         this.toolIntentDetector = toolIntentDetector;
         this.freshnessEvaluator = freshnessEvaluator;
